ab4029a0bb50e0ebd6f7c524457fd6afe4c5422f
Dawood AlOthman <Dawood@Dawoods-macbook.local>
2019-08-21 15:16:32 +0300

    Sameera Dawood

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from datetime import datetime
+
+class Page(models.Model):
+ 	title= models.CharField(max_length =30)
+ 	content = models.TextField()
+ 	last_update = models.DateTimeField(default=datetime.now())
+ 	
+
+ 	def __str__(self):
+ 		return self.title
+
+ 	def get_absolute_url(self):
+    	  return reverse('page-detail', args=[str(self.id)])
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+def list(request):
+	context= {
+
+		"plist": Page.objects.all(),
+	}
+	return render(request, "list.html", context)
+	
+
+def detail(request, page_id ):
+	context= {
+
+		"dlist": Page.objects.get(id=page_id),
+	}
+	return render(request, "details.html", context)
+	 
